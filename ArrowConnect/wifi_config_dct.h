60724456e577f41f99660ee689bfbca103639515
ARROWNAO\a89171 <cory.mast@arrow.com>
2018-03-01 15:53:07 -0700

    Removed ESC APs from config file.

--- ArrowConnect/wifi_config_dct.h
+++ ArrowConnect/wifi_config_dct.h
@@ -46,10 +46,10 @@ extern "C"
  ******************************************************/
 
 /* This is the soft AP used for device configuration */
-#define CONFIG_AP_SSID       "Mera-guest"
+#define CONFIG_AP_SSID       "WICED Config"
 #define CONFIG_AP_CHANNEL    1
 #define CONFIG_AP_SECURITY   WICED_SECURITY_WPA2_AES_PSK
-#define CONFIG_AP_PASSPHRASE "guest@Mera"
+#define CONFIG_AP_PASSPHRASE "12345678"
 
 /* This is the soft AP available for normal operation (if used)*/
 #define SOFT_AP_SSID         "WICED Device"
@@ -58,10 +58,8 @@ extern "C"
 #define SOFT_AP_PASSPHRASE   "WICED_PASSPHRASE"
 
 /* This is the default AP the device will connect to (as a client)*/
-//#define CLIENT_AP_SSID       "ECOMJEST"
-//#define CLIENT_AP_PASSPHRASE "@rr0wD3nv3r"
-#define CLIENT_AP_SSID       "HTC6545LVW 51 a9"//"Verizon-MiFi6620L-1620"
-#define CLIENT_AP_PASSPHRASE "9408401SM!"//"4e54b2a8"
+#define CLIENT_AP_SSID       "YOUR_AP_SSID"
+#define CLIENT_AP_PASSPHRASE "YOUR_AP_PASSPHRASE"
 #define CLIENT_AP_BSS_TYPE   WICED_BSS_TYPE_INFRASTRUCTURE
 #define CLIENT_AP_SECURITY   WICED_SECURITY_WPA2_AES_PSK
 #define CLIENT_AP_CHANNEL    1
